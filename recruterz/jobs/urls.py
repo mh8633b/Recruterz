4ada0fae2d8296c072a549aee670a7f6cae61126
mh8633b <mh8633b@gmail.com>
2021-05-21 23:39:28 -0700

    job content from db to jobs page 2

--- recruterz/jobs/urls.py
+++ recruterz/jobs/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('jobs', views.jobs, name='jobs'),
+    path('postjobs', views.postjobs, name='postjobs'),
     path('details', views.details, name='details')
 ]
 
--- recruterz/jobs/views.py
+++ recruterz/jobs/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import job
 
 # Create your views here.
 
 
 def jobs(request):
-    return render(request, "jobs.html")
-
+    jobs_content = job.objects.all()
+    return render(request, "jobs.html",{'jobs_content': jobs_content})
 
 def details(request):
     return render(request, "details.html")
+
+def postjobs(request):
+    # if request.method == "POST":
+    #     return render(request, "post_jobs.html")
+
+    return render(request, "post_job.html")
